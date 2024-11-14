cf06f3d7d39856b5d742a1e3e7c65853320b380d
Nikita Nallamothu <ninallam@microsoft.com>
2024-11-14 14:37:48 +0530

    Updated locust script for load testing

--- demo-scripts/azure-load-testing/contoso-traders-locust.py
+++ demo-scripts/azure-load-testing/contoso-traders-locust.py
@@ -1,45 +1,16 @@
 from locust import task, run_single_user
 from locust import FastHttpUser
+from locust import HttpUser, task, between
 
 
-class myrecording(FastHttpUser):
+class myrecording(HttpUser):
     host = "https://contoso-traders-ui2altig.azureedge.net"
     @task
     def t(self):
-        with self.client.request(
-            "GET",
-            "/",
-            catch_response=True,
-            name="Home Page"
-        ) as resp:
-            pass
-        with self.rest(
-            "GET",
-            "https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/products/?type=all-products",
-            headers={
-                "accept": "application/json, text/plain, */*",
-            },
-            name="All Products"
-        ) as resp:
-            pass
-        with self.rest(
-            "GET",
-            "https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/Products/search/xbox",
-            headers={
-                "accept": "application/json, text/plain, */*",
-            },
-            name="Search Xbox"
-        ) as resp:
-            pass
-        with self.rest(
-            "GET",
-            "https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/products/5",
-            headers={
-                "accept": "application/json, text/plain, */*",
-            },
-            name="Product Details"
-        ) as resp:
-            pass
-
+        self.client.get("/", name="Home Page")
+        self.client.get("https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/products/?type=all-products", name="Products")
+        self.client.get("https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/Products/search/xbox", name="Search Xbox")
+        self.client.get("https://contoso-traders-productsaltig.eastus2.cloudapp.azure.com/v1/products/5", name="Product Details")
+       
 if __name__ == "__main__":
     run_single_user(myrecording)
